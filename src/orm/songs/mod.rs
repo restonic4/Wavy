c3deb90049b7e4376fa4668c0a3e7f5470ed7797
restonic4 <marcosruizlerma@gmail.com>
2026-02-01 22:35:55 +0100

    allow 100mb

--- src/orm/songs/mod.rs
+++ src/orm/songs/mod.rs
@@ -2,6 +2,8 @@ pub mod models;
 pub mod repository;
 pub mod handlers;
 pub mod upload;
+
+use axum::extract::DefaultBodyLimit;
 use axum::Router;
 use axum::routing::{get, post};
 use crate::state::AppState;
@@ -12,4 +14,5 @@ pub fn router() -> Router<AppState> {
         .route("/songs/upload", post(upload::upload_song))
         .route("/songs/{id}", get(handlers::get_song).post(handlers::update_song).delete(handlers::delete_song))
         .route("/songs/{id}/image", get(handlers::get_song_image))
+        .layer(DefaultBodyLimit::max(100 * 1024 * 1024))
 }
