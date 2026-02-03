0280918eabe0ed08ce2245d62a9aaf7147ef5cca
restonic4 <marcosruizlerma@gmail.com>
2026-02-03 16:45:15 +0100

    [-] Fixed typo

--- backend/migrations/5_create_users_table.sql
+++ backend/migrations/5_create_users_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL,
     artist_id INTEGER,
     role TEXT NOT NULL DEFAULT 'user', -- 'user' or 'admin'
-    total_listen_time INTEGER NOT NULL DEFAULT 0;
+    total_listen_time INTEGER NOT NULL DEFAULT 0,
     FOREIGN KEY (artist_id) REFERENCES artists(id) ON DELETE SET NULL
 );
